f9f165558eff8ac857bdf4309a145a8e5e6f9043
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-06-18 11:14:17 -0300

    Estilos globais

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
+import GlobalStyle from '../src/styles/global';
+
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
