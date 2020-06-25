3d86f31c0f894b277621fc5e025ce1bdd877e2ab
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-06-25 09:54:38 -0300

    Configurando Redux-Saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import creatSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const enhacer = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+const sagaMiddleware = creatSagaMiddleware();
+
+const enhacer = process.env.NODE_ENV === 'development' 
+  ? compose(
+      console.tron.createEnhancer(),
+      applyMiddleware(sagaMiddleware)
+    )
+  : applyMiddleware(sagaMiddleware);
 
 
 const store = createStore(rootReducer, enhacer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
