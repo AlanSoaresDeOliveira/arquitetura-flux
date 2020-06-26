f5b1783ff7d4e0b6223810adf4b72f9a14f52d63
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-06-26 18:16:50 -0300

    Navegando no Saga

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {ToastContainer} from 'react-toastify';
 
-import GlobalStyle from '../src/styles/global';
-
 import './config/ReactotronConfig';
 
-import Routes from './routes';
+import GlobalStyle from '../src/styles/global';
+
 import Header from '../src/components/Header';
+import Routes from './routes';
 
-
+import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider  store={store}>    
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer  autoClose={3000}/>
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
