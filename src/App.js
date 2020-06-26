4d86a0ee350d14af2c8382f2b0652dc486eaef70
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-06-26 17:29:27 -0300

    Adicionando Toast

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
+import {ToastContainer} from 'react-toastify';
+
 import GlobalStyle from '../src/styles/global';
+
 import './config/ReactotronConfig';
 
 import Routes from './routes';
@@ -17,6 +20,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer  autoClose={3000}/>
       </BrowserRouter>
     </Provider>
   );
